910b8a37ac945147973a145414cfe23f0c4958ae
comimome <olee5889@gmail.com>
2024-02-10 20:59:48 +0900

    sosuu

---
+++ library/math/sieve_of_eratosthenes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define FOR(i, m, n) for (ll i = m; i <= n; ++i)
+template<class t> using vc = vector<t>;
+
+// 1 以上 N 以下の整数が素数かどうかを返す
+vc<bool> Eratosthenes(ll N) {
+    vc<bool> isprime(N+1, true);
+    isprime[1] = false;
+    FOR(p, 2, N){
+        if(!isprime[p])continue;
+        for(ll q=p*2; q<=N; q+=p){
+            isprime[q] = false;
+        }
+    }
+    return isprime;
+}
