5934a32fabc8f3c31ab10aec4577569984ecf2a3
comimome <olee5889@gmail.com>
2024-02-26 14:26:22 +0900

    2

---
+++ abc/abc040/c/c.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+#define FOR(i, m, n) for (ll i = (m); i <= (n); ++i)
+#define FORR(i, m, n) for (ll i = (m); i >= (n); --i)
+#define REP(i, n) for (ll i = 0; i < (n); ++i)
+#define REPR(i, n) for (ll i = (n); i >= 0; --i)
+#define ALL(v) (v).begin(),(v).end()
+#define RV(v) reverse((v).begin(), (v).end())
+#define BS(v, key) binary_search((v).begin(), (v).end(), key)
+#define LB(v, key) lower_bound((v).begin(), (v).end(), key)
+#define UB(v, key) upper_bound((v).begin(), (v).end(), key)
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+const int mod93 = 998244353;
+const int mod17 = 1000000007;
+template<class t> using vc = vector<t>;
+template<class t> using vvc = vc<vc<t>>;
+template<class t> using vvvc = vc<vc<vc<t>>>;
+template<class t> using pq = priority_queue<t>;
+template<class t> using pqg = priority_queue<t, vc<t>, greater<t>>;
+template<class t, class u> using um = unordered_map<t, u>;
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
+template<size_t t> using mint = Fp<t>;
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; } // 最大公約数
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b; } // 最小公倍数
+void Yes(bool f = 1) { cout<<(f ? "Yes" : "No")<<endl; }
+void No(bool f = 1) { Yes(!f); }
+
+// DP
+// 移動コストの最小化
+void solve(){
+    int n;
+    cin>>n;
+    vc<ll> a(n);
+    REP(i,n)cin>>a[i];
+    vc<ll> dp(n,0);
+    dp[1]=abs(a[1]-a[0]);
+    FOR(i,2,n-1){
+        dp[i]=min(dp[i-2]+abs(a[i]-a[i-2]),dp[i-1]+abs(a[i]-a[i-1]));
+    }
+    cout<<dp[n-1]<<endl;
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cout << fixed << setprecision(20);
+    int t=1;
+    // cin >> t;
+    while(t--)solve();
+    return 0;
+}
--- library/dp/knapsack.cpp
+++ library/dp/knapsack.cpp
@@ -5,8 +5,14 @@ using ll = long long;
 #define REP(i, n) for (ll i = 0; i < (n); ++i)
 template<class t> using vc = vector<t>;
 
-// 個数制限なしナップサック問題
+// 個数制限なし最大和ナップサック問題
 // O(NW)
 vc<ll> knapsack(const vc<ll> &w, const vc<ll> &v, const ll &W) {
-    vc<ll> dp(W + 1)
+    vvc<ll> dp(w.size(), vc<ll>(W+1, 0));
+    REP(i, n) {
+        REP(j, W+1) {
+            if(j >= w[i]) dp[i+1][j] = max(dp[i][j-w[i]] + v[i], dp[i][j]);
+            else dp[i+1][j] = dp[i][j];
+        }
+    }
 }
